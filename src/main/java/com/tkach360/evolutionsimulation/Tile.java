0855f6f084b68e3ef5f8baa45040da11b3dfc088
Ткаченко Алексей <tkacenk002@mail.ru>
2024-06-05 15:59:06 +0700

    сделал Tile и TileMap

---
+++ src/main/java/com/tkach360/evolutionsimulation/Tile.java
@@ -0,0 +1,67 @@
+package com.tkach360.evolutionsimulation;
+
+import java.util.Objects;
+
+public class Tile {
+
+    private double cx; // координата X
+    private double cy; // координата Y
+
+    private double soilEnergy; // энергия в почве тайла
+    private double lighting; // освещенность тайла
+
+    public Tile(double cx, double cy, double soilEnergy, double lighting) {
+        this.cx = cx;
+        this.cy = cy;
+        this.soilEnergy = soilEnergy;
+        this.lighting = lighting;
+    }
+
+    public double getSoilEnergy() {
+        return soilEnergy;
+    }
+
+    public void setSoilEnergy(double soilEnergy) {
+        this.soilEnergy = soilEnergy;
+    }
+
+    public double getLighting() {
+        return lighting;
+    }
+
+    public void setLighting(double lighting) {
+        this.lighting = lighting;
+    }
+
+    public double getCx() {
+        return cx;
+    }
+
+    public double getCy() {
+        return cy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tile tile = (Tile) o;
+        return Double.compare(cx, tile.cx) == 0 && Double.compare(cy, tile.cy) == 0 && Double.compare(soilEnergy, tile.soilEnergy) == 0 && Double.compare(lighting, tile.lighting) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cx, cy, soilEnergy, lighting);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Tile{");
+        sb.append("cx=").append(cx);
+        sb.append(", cy=").append(cy);
+        sb.append(", soilEnergy=").append(soilEnergy);
+        sb.append(", lighting=").append(lighting);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/tkach360/evolutionsimulation/TileMap.java
@@ -0,0 +1,51 @@
+package com.tkach360.evolutionsimulation;
+
+import javafx.scene.canvas.Canvas;
+
+/**этот класс еще не закончен так что не торопись с equals, hashCode и т.д.\
+
+ * над размером поля (в тайлах) все же подумай
+ * необходимо потом редактировать soilEnetgy и lighting
+ */
+
+public class TileMap {
+
+    public static final double TILE_SIDE = 5; // размер тайла
+    public static final double EDGE_DISTANCE = 0.5; // над этим подумать
+
+    private int countTiles;
+    private Canvas canvas;
+    private Tile[][] tiles;
+
+    public TileMap(Canvas canvas) {
+        this.canvas = canvas;
+
+        int countColumns = (int)(canvas.getWidth() / TILE_SIDE);
+        int countRows = (int)(canvas.getHeight() / TILE_SIDE);
+
+        this.tiles = new Tile[countColumns][countRows];
+
+//        заполняем мировое поле тайлами
+        for(int i=0;i<countColumns;i++){
+            for(int j=0;j<countRows;j++){
+                this.tiles[i][j] = new Tile(i*TILE_SIDE, j*TILE_SIDE, 10, 10); // необходимо потом редактировать soilEnetgy и lighting
+            }
+        }
+
+        this.countTiles = countColumns * countRows;
+    }
+
+    public int getCountTiles() {
+        return countTiles;
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+
+    public Tile[][] getTiles() {
+        return tiles;
+    }
+
+
+}
