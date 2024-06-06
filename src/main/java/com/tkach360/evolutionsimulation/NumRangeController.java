8b3b59d4ff65ca8a413b9b92b6837dbcb416b84c
Ткаченко Алексей <tkacenk002@mail.ru>
2024-06-06 15:04:57 +0700

    сделал NumRangeController для контроля за промежутками

---
+++ src/main/java/com/tkach360/evolutionsimulation/NumRangeController.java
@@ -0,0 +1,17 @@
+package com.tkach360.evolutionsimulation;
+
+/** класс содержащий методы для установки значения в определенном диапазоне */
+public class NumRangeController {
+
+    public static int setInRange(int num, int minValue, int maxValue){
+        if(num < minValue) num = minValue;
+        if(num > maxValue) num = maxValue;
+        return num;
+    }
+
+    public static double setInRange(double num, double minValue, double maxValue){
+        if(num < minValue) num = minValue;
+        if(num > maxValue) num = maxValue;
+        return num;
+    }
+}
