fc843692d9f48ce8fc41e260ac7a54a4134ed605
Ткаченко Алексей <tkacenk002@mail.ru>
2024-06-12 17:34:04 +0700

    выделил BotNode в отдельный класс

---
+++ src/main/java/com/tkach360/evolutionsimulation/BotNode.java
@@ -0,0 +1,43 @@
+package com.tkach360.evolutionsimulation;
+
+public class BotNode{
+    private Bot bot;
+    private int next;
+    private int prev;
+
+    public BotNode() {
+        this.bot = null;
+        this.next = 0;
+        this.prev = 0;
+    }
+
+    public BotNode(Bot bot, int next, int prev) {
+        this.bot = bot;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public Bot getBot() {
+        return bot;
+    }
+
+    public void setBot(Bot bot) {
+        this.bot = bot;
+    }
+
+    public int getNext() {
+        return next;
+    }
+
+    public void setNext(int next) {
+        this.next = next;
+    }
+
+    public int getPrev() {
+        return prev;
+    }
+
+    public void setPrev(int prev) {
+        this.prev = prev;
+    }
+}
