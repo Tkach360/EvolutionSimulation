de81548d5dda52338312ba6211b0fef68b0f767a
Ткаченко Алексей <tkacenk002@mail.ru>
2024-06-17 20:20:30 +0700

    заменил AbstractBehavior на IBehavior

--- src/main/java/com/tkach360/evolutionsimulation/AbstractBehavior.java
+++
@@ -1,5 +0,0 @@
-package com.tkach360.evolutionsimulation;
-
-public abstract class AbstractBehavior {
-    public abstract void doSomething(Bot bot);
-}
---
+++ src/main/java/com/tkach360/evolutionsimulation/IBehavior.java
@@ -0,0 +1,5 @@
+package com.tkach360.evolutionsimulation;
+
+public interface IBehavior {
+    int decide(Bot bot);
+}
